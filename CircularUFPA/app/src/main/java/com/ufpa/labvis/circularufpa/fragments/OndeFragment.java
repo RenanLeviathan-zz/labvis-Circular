cd6b348beef700057dd6bc47120bcdc50b9f7889
RenanLeviathan <renan.leviathan@gmail.com>
2016-06-13 11:34:26 -0300

    Circular
    projeto circular ufpa

---
+++ CircularUFPA/app/src/main/java/com/ufpa/labvis/circularufpa/fragments/OndeFragment.java
@@ -0,0 +1,25 @@
+package com.ufpa.labvis.circularufpa.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ufpa.labvis.circularufpa.R;
+public class OndeFragment extends Fragment{
+
+    public OndeFragment(){
+
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onde, container, false);
+    }
+}
